4693bbe51ba0b8792ac0c24a7152e1556157579d
SiddharthPatil <siddharthpatilcourses@gmail.com>
2026-07-04 23:53:08 +0530

    Add swagger and CORS

--- eCommerce.API/Program.cs
+++ eCommerce.API/Program.cs
@@ -28,6 +28,24 @@ namespace eCommerce.API
             //FluentValidations
             builder.Services.AddFluentValidationAutoValidation();
 
+            //Add API explorer services
+            builder.Services.AddEndpointsApiExplorer();
+
+            //Add swagger generation services to create swagger specification
+            builder.Services.AddSwaggerGen();
+
+            //Add CORS policy
+            builder.Services.AddCors(options=>
+            {
+                options.AddDefaultPolicy(builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                           .AllowAnyMethod()
+                           .AllowAnyHeader();
+                });
+            }
+            );
+
             //Build the web application
             var app = builder.Build();
 
@@ -36,6 +54,9 @@ namespace eCommerce.API
 
             //Routing
             app.UseRouting();
+            app.UseSwagger();//Adds endpoint that can serve swagger.json
+            app.UseSwaggerUI();//Adds swagger UI to view the swagger.json
+            app.UseCors();
 
             //Auth
             app.UseAuthentication();
